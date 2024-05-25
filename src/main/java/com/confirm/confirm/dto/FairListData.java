dcf5f902957b4e79b525b2e2c70a0de60f2e902a
G5py <gspy0231@gmail.com>
2024-05-25 19:21:40 +0900

    feat : FairListData DTO 추가.

---
+++ src/main/java/com/confirm/confirm/dto/FairListData.java
@@ -0,0 +1,15 @@
+package com.confirm.confirm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FairListData {
+    private String imagePath;
+    private String fairName;
+    private String organizerName;
+    private String keyword;
+    private String location;
+    private String time;
+}
