cc1c8633645ddbe7f24c4e73ab4a78e327958418
G5py <gspy0231@gmail.com>
2024-05-25 19:20:49 +0900

    feat : 행사 관련 로직을 수행하는 FairService 클래스의 stub 코드 추가.

---
+++ src/main/java/com/confirm/confirm/service/FairService.java
@@ -0,0 +1,23 @@
+package com.confirm.confirm.service;
+
+import com.confirm.confirm.dto.FairListData;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// stub 클래스이며 이후 추가 예정.
+@Service
+public class FairService {
+    public List<FairListData> getFairList() {
+        return Arrays.asList(new FairListData(
+                "images/firm1.png",
+                "LX 지리데이터 행사",
+                "LX 한국국토정보공사",
+                "지리, 공간 데이터",
+                "전주시",
+                "6월 25일"
+        ));
+    }
+}
