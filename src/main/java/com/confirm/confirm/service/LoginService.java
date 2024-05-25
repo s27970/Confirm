ff7a63e59a42417b7bfec5b325fad056f713d05c
G5py <gspy0231@gmail.com>
2024-05-25 20:26:19 +0900

    fix : LoginData -> Login 이름 변경에 의한 오류 수정.

--- src/main/java/com/confirm/confirm/service/LoginService.java
+++ src/main/java/com/confirm/confirm/service/LoginService.java
@@ -1,13 +1,13 @@
 package com.confirm.confirm.service;
 
 
-import com.confirm.confirm.dto.LoginData;
+import com.confirm.confirm.dto.Login;
 import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    public Cookie login(LoginData loginData) {
+    public Cookie login(Login loginData) {
         // 이 코드는 빌드를 깨지 않기 위한 stub 코드임. 추후 구현이 필요함.
         Cookie cookie = new Cookie("session", "loginCookie");
         cookie.setMaxAge(60*60);
