02c414c2e171d961237cdd69f0bd07de475a1fd5
Hj11 <hojin2661@naver.com>
2024-05-27 16:12:08 +0900

    메인 페이지 경로 수정-최호진

--- src/main/java/com/confirm/confirm/dto/PostingCardDTO.java
+++ src/main/java/com/confirm/confirm/dto/PostingCardDTO.java
@@ -13,51 +13,5 @@ public class PostingCardDTO {
     private String position1;
     private String position2;
 
-    public String getImagePath() {
-        return imagePath;
-    }
 
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCompanySize() {
-        return companySize;
-    }
-
-    public void setCompanySize(String companySize) {
-        this.companySize = companySize;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
-
-    public String getPosition1() {
-        return position1;
-    }
-
-    public void setPosition1(String position1) {
-        this.position1 = position1;
-    }
-
-    public String getPosition2() {
-        return position2;
-    }
-
-    public void setPosition2(String position2) {
-        this.position2 = position2;
-    }
 }
