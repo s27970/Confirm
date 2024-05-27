499dc30eeb9cacc24a4a1546a15d7ed98b8d9903
YeongJae Son <s27970@naver.com>
2024-05-27 18:11:14 +0900

    Lombok 적용 및 수정

--- src/main/java/com/confirm/confirm/dto/UserDTO.java
+++ src/main/java/com/confirm/confirm/dto/UserDTO.java
@@ -1,50 +1,15 @@
 package com.confirm.confirm.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private String userId;
     private String userName;
     private String userSchool;
     private String userCareer;
     private String userPreviousJobCategory;
-
-    // Getters and Setters
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserSchool() {
-        return userSchool;
-    }
-
-    public void setUserSchool(String userSchool) {
-        this.userSchool = userSchool;
-    }
-
-    public String getUserCareer() {
-        return userCareer;
-    }
-
-    public void setUserCareer(String userCareer) {
-        this.userCareer = userCareer;
-    }
-
-    public String getUserPreviousJobCategory() {
-        return userPreviousJobCategory;
-    }
-
-    public void setUserPreviousJobCategory(String userPreviousJobCategory) {
-        this.userPreviousJobCategory = userPreviousJobCategory;
-    }
 }
