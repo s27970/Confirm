c15947300ecad3da3c1085fd013ea7f8f84b51a1
Hj11 <hojin2661@naver.com>
2024-05-29 18:05:47 +0900

    공고등록 페이지 리펙토링

--- src/main/java/com/confirm/confirm/controller/MainPageController.java
+++ src/main/java/com/confirm/confirm/controller/MainPageController.java
@@ -33,11 +33,6 @@ public class MainPageController {
 
 
 
-    @GetMapping("/jobRec")
-    public String jobRec() {
-        return "jobRec"; // jobRec.html로 이동
-    }
-
     @GetMapping("/logib")
     public String login() {
         return "login";
--- src/main/java/com/confirm/confirm/controller/RecPageController.java
+++ src/main/java/com/confirm/confirm/controller/RecPageController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RecPageController {
-    @GetMapping("/rec/jobRec")
+    @GetMapping("/jobRec")
     public String jobRec() {
         return "jobRec"; // jobRec.html로 이동
     }
 
-    @GetMapping("/rec/eventRec")
+    @GetMapping("/eventRec")
     public String eventRec() {
         return "eventRec"; // eventRec.html로 이동
     }
